56d3286bbb440559e149a5d895756f41de561e27
nam1008 <huynam23456.mk@gmail.com>
2020-04-27 20:12:18 +0700

    Commit

--- js/app.js
+++ js/app.js
@@ -1,39 +1,23 @@
 function checkLength(e, minLength) {
-  // Declare function
-  var el, elMsg; // Declare variables
-  if (!e) {
-    // If event object doesn't exist
-    e = window.event; // Use IE fallback
-  }
-  el = e.target || e.srcElement; // Get target of event
-  elMsg = document.getElementById('feedback'); // Get its next sibling
+  var el, elMsg;
+  el = e.target || e.srcElement;
+  elMsg = document.getElementById("feedback");
 
   if (el.value.length < minLength) {
-    // If length is too short set msg
     elMsg.innerHTML = "Username must be " + minLength + " characters or more";
   } else {
-    // Otherwise
-    elMsg.innerHTML = ""; // Clear message
+    elMsg.innerHTML = "";
   }
 }
 
-var elUsername = document.getElementById("username"); // Get username input
+var elUsername = document.getElementById("username");
 if (elUsername.addEventListener) {
   // If event listener supported
   elUsername.addEventListener(
     "blur",
     function (e) {
-      // On blur event
-      // NOTE: This function is checkLength() - not checkUsername()
-      checkLength(e, 5); // Call checkLength()
+      checkLength(e, 5);
     },
     false
-  ); // Capture in bubble phase
-} else {
-  // Otherwise
-  elUsername.attachEvent("onblur", function (e) {
-    // IE fallback onblur
-    // NOTE: This function is checkLength() - not checkUsername()
-    checkLength(e, 5); // Call checkLength()
-  });
+  );
 }
